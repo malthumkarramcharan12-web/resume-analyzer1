86d632f9c26c6c3ee3ba1773daa1abcea38eec4f
malthumkarramcharan12-web <malthumkarramcharan12@gmail.com>
2026-03-25 18:05:55 +0530

    Add SKILLS list with programming languages and tools

---
+++ skills.py
@@ -0,0 +1,7 @@
+SKILLS = [
+    "python", "java", "c++", "sql",
+    "machine learning", "deep learning", "nlp",
+    "flask", "django", "html", "css",
+    "javascript", "react", "node",
+    "docker", "aws", "git"
+]
